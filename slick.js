e708ca1773182fff5f9abcb5ed43deac06a1a7f2
HelenaUa <110732166+HelenaUa@users.noreply.github.com>
2023-10-23 19:47:02 +0300

    add reviews

---
+++ slick.js
@@ -0,0 +1,20 @@
+$('.opacity').slick({
+    infinite: true,
+    arrows: false,
+    fade: true,
+    slidesToShow: 1,
+    autoplay: true,
+    centerMode: true,
+    autoplaySpeed: 2000,
+    draggable: false,
+  });
+  
+  $('.one-time').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 9000,
+  });
