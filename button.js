234a2e0f862f5e507e6b568b17faa956248b28d5
HelenaUa <110732166+HelenaUa@users.noreply.github.com>
2023-10-29 18:59:09 +0200

    add js read-more-btn

--- button.js
+++ button.js
@@ -1,19 +1,32 @@
-var btn = $('#button');
+// var btn = $('#button');
 
-$(window).scroll(function () {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
+// $(window).scroll(function () {
+//   if ($(window).scrollTop() > 300) {
+//     btn.addClass('show');
+//   } else {
+//     btn.removeClass('show');
+//   }
+// });
+
+// $('#button').click(function () {
+//   $('html, body').animate({ scrollTop: 0 }, 100);
+//   return false;
+// });
+
+
+
+// Обираємо всі кнопки з класом .read-more-btn
+const readMoreButtons = document.querySelectorAll('.read-more-btn');
+readMoreButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    const aboutText = button.previousElementSibling; 
+    aboutText.classList.toggle('hidden');
+  }); 
 });
 
-$('#button').click(function () {
-  $('html, body').animate({ scrollTop: 0 }, 100);
-  return false;
-});
-
+// Якщо одна кнопка з класом .read-more-btn
 // document.querySelector('.read-more-btn').addEventListener('click', function() {
-//   const aboutText = document.querySelector('.about-text');
+//   const aboutText = document.querySelector('.read-text');
 //   aboutText.classList.toggle('hidden');
 // });
+
